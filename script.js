39c2ea52c3c18bed3fe95dfbb1ac92a39881a389
ToothedYew <david.grow68@gmail.com>
2025-11-19 21:55:11 +0900

    Start to increment score when round is played

--- script.js
+++ script.js
@@ -21,18 +21,22 @@ function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         return `It's a draw! ${humanChoice} equals ${computerChoice}`
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        ++humanScore
         return `You win! Rock beats scissors!`
     } else if (humanChoice === "rock" && computerChoice === "paper") {
+        ++computerScore
         return `You lose! Paper beats rock!`
     } else if (humanChoice === "paper" && computerChoice === "rock") {
+        ++humanScore
         return `You win! Paper beats rock!`
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        ++computerScore
         return `You lose! Scissors beats paper!!`
     }
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
 
-console.log(playRound(humanSelection, computerSelection));
+// console.log(playRound(humanSelection, computerSelection));
