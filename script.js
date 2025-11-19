c0bb670f1fb7fe12bfb2709bae315fc671bafc93
ToothedYew <david.grow68@gmail.com>
2025-11-19 21:46:33 +0900

    Create playRound function

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function getHumanChoice() {
     return choice.toLowerCase()
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return `It's a draw! ${humanChoice} equals ${computerChoice}`
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        return `You win! Rock beats scissors!`
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        return `You lose! Paper beats rock!`
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        return `You win! Paper beats rock!`
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        return `You lose! Scissors beats paper!!`
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
